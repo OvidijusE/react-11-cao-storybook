7c130ef53a9af1a062c0c0e0b2f52eb09d7d1b7d
Ovidijus <ovidijus.e@gmail.com>
2022-06-23 08:34:47 +0300

    input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.css';
 import Button from './components/Button/Button';
+import Hero from './components/Hero/Hero';
+import Notification from './components/Notification/Notification';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello again world</h1>
-      <Button size='large'>Large btn</Button>
-      <Button variant='secondary' size='small'>
-        Small btn
-      </Button>
-      <Button>Click me</Button>
-      <Button>ha</Button>
+      <h1>Cao</h1>
+      <div className='buttons'>
+        <Button variant='primary' size='large'>
+          Primary
+        </Button>
+        <Button variant='secondary' size='large'>
+          Secondary
+        </Button>
+      </div>
 
-      <Button customFontSize={30}>customFontSize30</Button>
+      <div className='alerts'>
+        <Notification icon={'fa-exclamation-circle'} variant='danger' cancel={'fa-times-circle-o'}>
+          This is danger alert
+        </Notification>
+        <Notification icon={'fa-question-circle-o'} variant='warning' cancel={'fa-times-circle-o'}>
+          This is warning alert
+        </Notification>
+        <Notification icon={'fa-info-circle'} variant='info' cancel={'fa-times-circle-o'}>
+          This is info alert
+        </Notification>
+        <Notification icon={'fa-check-circle-o'} variant='success' cancel={'fa-times-circle-o'}>
+          This is success alert
+        </Notification>
+      </div>
+
+      <div className='hero'>
+        <Hero type='primary' title='Primary hero' subtitle='Primary subtitle' />
+      </div>
     </div>
   );
 }
