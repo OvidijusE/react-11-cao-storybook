4fa5e2a6683fae9d88802809d5f3ec26fcf6ef1d
Ovidijus <ovidijus.e@gmail.com>
2022-06-23 09:45:20 +0300

    darkTextArea

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,22 @@
 import css from './Input.module.css';
 import PropTypes from 'prop-types';
 
-function Input({ color = 'light' }) {
-  return <input className={`${css.input} ${css[color]}`} />;
+function Input({ color = 'light', type = 'text', children, ...rest }) {
+  if (type === 'textarea') {
+    return (
+      <textarea className={`${css.input} ${css.txa} ${css[color]}`} {...rest}>
+        {children}
+      </textarea>
+    );
+  }
+
+  return <input className={`${css.input} ${css[color]}`} {...rest} />;
 }
 
 Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['light', 'dark']),
+  type: PropTypes.oneOf(['text', 'textarea']),
+  value: PropTypes.string,
 };
 export default Input;
--- src/components/Input/Input.stories.jsx
+++ src/components/Input/Input.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Input from './Input';
 
 export default {
-  title: 'Input',
+  title: 'Form/Input',
   component: Input,
 };
 
@@ -11,4 +11,24 @@ const Template = (args) => <Input {...args} />;
 export const MainInput = Template.bind({});
 MainInput.args = {
   placeholder: 'Enter text here',
+  color: 'light',
+};
+
+export const DarkInput = Template.bind({});
+DarkInput.args = {
+  ...MainInput.args,
+  color: 'dark',
+};
+
+export const TextArea = Template.bind({});
+TextArea.args = {
+  ...MainInput.args,
+  type: 'textarea',
+};
+
+export const DarkTextArea = Template.bind({});
+DarkTextArea.args = {
+  ...TextArea.args,
+  type: 'textarea',
+  color: 'dark',
 };
